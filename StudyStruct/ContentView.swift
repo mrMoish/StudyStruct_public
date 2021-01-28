9158299896cc69e33ec12d023633ed5e153ac93d
Vasilii <v.y.moish@gmail.com>
2021-01-28 12:26:43 +0300

    start

---
+++ StudyStruct/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyStruct
+//
+//  Created by Vasilii Moish on 28.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! I'am Vasilii")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyStruct/StudyStructApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyStructApp.swift
+//  StudyStruct
+//
+//  Created by Vasilii Moish on 28.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct StudyStructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
